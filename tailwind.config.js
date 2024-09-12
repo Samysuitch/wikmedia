3021572d0768be5a51d5a89a5b61cb3159feeae0
Iyakaremye Samuel <samueliyakaremye120@gmail.com>
2024-09-12 16:47:27 +0200

    my files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './*.html',
+    './css/*.css',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        pink: 'rgb(214 47 41)',
+      },
+    },
+  },
+  plugins: [],
+}
+
